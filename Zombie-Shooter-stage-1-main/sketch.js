5f05f0bde14ba092315fa87e50ee869d169c57d0
yuvraj9742 <75372064+yuvraj9742@users.noreply.github.com>
2021-09-16 16:41:13 +0530

    Add files via upload

---
+++ Zombie-Shooter-stage-1-main/sketch.js
@@ -0,0 +1,70 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var heart1,heart2,heart3, heart1Img,heart2Img,heart3Img
+var life=3
+var zombie, zombieImg, zombieGroup
+var score=0
+var bullets=50
+var gameState="start"
+var loseSound, windSound, explodingSound
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+
+drawSprites();
+
+}
